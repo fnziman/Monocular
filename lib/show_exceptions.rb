febc64e543797336dcee837e4fcd3e69e147c7b0
Frankie Ziman <FNZiman@gmail.com>
2017-09-05 17:39:00 -0400

    created exceptions middleware

--- lib/show_exceptions.rb
+++ lib/show_exceptions.rb
@@ -1,15 +1,26 @@
 require 'erb'
 
 class ShowExceptions
+  attr_reader :app
   def initialize(app)
+    @app = app
   end
 
   def call(env)
+    @app.call(env)
+  rescue Exception => e
+    render_exception(e)
   end
 
   private
 
   def render_exception(e)
+    @error = e
+    file = File.read("lib/templates/rescue.html.erb")
+    content = ERB.new(file).result(binding)
+    res = Rack::Response.new
+    res['Content-Type'] = 'text/html'
+    res.write(content)
+    res.finish
   end
-
 end
