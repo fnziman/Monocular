4e3318800af79a5c41d57360f8bb9a72768ccd2c
Frankie Ziman <FNZiman@gmail.com>
2017-09-06 08:19:55 -0400

    created example using favorite shows

---
+++ show.rb
@@ -0,0 +1,75 @@
+require 'rack'
+require_relative './lib/controller_base'
+require_relative './lib/router'
+require 'byebug'
+
+class Show
+  attr_reader :title, :personal_rating
+
+  def self.all
+    @shows ||= []
+  end
+
+  def initialize(params = {})
+    @title = params["title"]
+    @personal_rating = params["personal_rating"]
+  end
+
+  def errors
+    @errors ||= []
+  end
+
+  def valid?
+    errors << "Title can't be blank" unless title.present?
+    errors << "Personal Rating can't be blank" unless personal_rating.present?
+    errors.empty?
+  end
+
+  def save
+    return false unless valid?
+    Show.all << self
+    true
+  end
+
+end
+
+class ShowsController < ControllerBase
+  def new
+    @show = Show.new
+    render :new
+  end
+
+  def create
+    @show = Show.new(params["show"])
+
+    if @show.save
+      flash[:notice] = "New Show Added!"
+      redirect_to "/shows"
+    else
+      flash.now[:errors] = @show.errors
+      render :new
+    end
+  end
+
+  def index
+    @shows = Show.all
+    render :index
+  end
+end
+
+router = Router.new
+
+router.draw do
+  get Regexp.new("^/shows/new$"), ShowsController, :new
+  post Regexp.new("^/shows$"), ShowsController, :create
+  get Regexp.new("^/shows$"), ShowsController, :index
+end
+
+app = Proc.new do |env|
+  req = Rack::Request.new(env)
+  res = Rack::Response.new
+  router.run(req, res)
+  res.finish
+end
+
+Rack::Server.start(app: app, Port: 3000)
